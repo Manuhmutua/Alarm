442a5fdb423eb3869100d5eec1f4be7ccd70b679
Manuh.__ <manuh.mutuah@gmail.com>
2021-03-01 11:57:13 +0300

    Added reschedule alarms

--- app/src/main/java/com/example/alamapplication/service/RescheduleAlarmsService.kt
+++ app/src/main/java/com/example/alamapplication/service/RescheduleAlarmsService.kt
@@ -1,1 +1,36 @@
 package com.example.alamapplication.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.annotation.Nullable
+import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.observe
+import com.example.alamapplication.viewmodel.AlarmViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RescheduleAlarmsService : LifecycleService() {
+
+    lateinit var viewModel: AlarmViewModel
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+
+        viewModel.getAlarms()?.observe(this) { alarms ->
+            for (a in alarms) {
+                if (a.started) {
+                    a.schedule(applicationContext)
+                }
+            }
+        }
+
+        return Service.START_STICKY
+    }
+
+    @Nullable
+    override fun onBind(intent: Intent): IBinder? {
+        super.onBind(intent)
+        return null
+    }
+}
